465fdc754baba4e540bc68d861782755b3e58b30
Bidya2003 <bidyajanimaji2003@gmail.com>
2025-10-18 10:26:56 +0530

    Initial commit: AI Resume Analyzer with dark glassmorphic UI

---
+++ courses.py
@@ -0,0 +1,226 @@
+# courses.py
+
+# =========================
+# Data Science Courses
+# =========================
+ds_course = [
+    ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
+    ['Data Science Foundations: Fundamentals by LinkedIn','https://www.linkedin.com/learning/data-science-foundations-fundamentals-5'],
+    ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+    ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+    ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+    ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017']
+]
+
+# =========================
+# Web Development Courses
+# =========================
+web_course = [
+    ['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+    ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+    ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+    ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+    ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+    ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+    ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+    ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+    ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+    ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']
+]
+
+# =========================
+# ml Development Courses
+# =========================
+ml_course = [
+    ['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+    ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+    ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+    ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230'],
+    ['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+    ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+    ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+    ['Deep Learning Specialization by Andrew NG','https://www.coursera.org/specializations/deep-learning'],
+    ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+    ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+    ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+    ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230'],
+    ['AI For Everyone by Andrew NG [Free]','https://www.coursera.org/learn/ai-for-everyone'],
+    ['Applied Data Science with Python Specialization','https://www.coursera.org/specializations/data-science-python']
+]
+
+# =========================
+# fullstack Development Courses
+# =========================
+fullstack_course = [
+    ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+    ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+    ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+    ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+    ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+    ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019'],
+    ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+    ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+    ['MERN Stack Front to Back','https://www.udemy.com/course/mern-stack-front-to-back/'],
+    ['Advanced Web Development with React & Node.js','https://www.coursera.org/specializations/full-stack-react-node']
+]
+
+
+
+# =========================
+# frontend Development Courses
+# =========================
+frontend_course = [
+    ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+    ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+    ['HTML, CSS, JS Crash Course [Free]','https://youtu.be/UB1O30fR-EE'],
+    ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+    ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019'],
+    ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+    ['JavaScript & ES6 Advanced Concepts','https://www.udemy.com/course/advanced-javascript-concepts/']
+]
+
+
+
+# =========================
+# backend Development Courses
+# =========================
+backend_course = [
+    ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+    ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+    ['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+    ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+    ['REST API Development with Flask and Python','https://www.udemy.com/course/rest-api-flask-and-python/'] 
+]
+
+
+# =========================
+# Artificial Intelligence & Advanced ML Courses
+# =========================
+ai_course = [
+    # Deep Learning
+    ['Deep Learning Specialization by Andrew Ng','https://www.coursera.org/specializations/deep-learning'],
+    ['Deep Learning with Python and PyTorch','https://www.udemy.com/course/deeplearning/'],
+    ['Convolutional Neural Networks for Visual Recognition','https://www.coursera.org/learn/convolutional-neural-networks'],
+    ['TensorFlow Developer Certificate in 2025','https://www.udemy.com/course/tensorflow-developer-certificate/'],
+
+    # Natural Language Processing (NLP)
+    ['Natural Language Processing Specialization','https://www.coursera.org/specializations/natural-language-processing'],
+    ['NLP with Python for Machine Learning Essential Training','https://www.linkedin.com/learning/nlp-with-python-for-machine-learning-essential-training'],
+    ['Transformers for NLP: BERT, GPT, T5','https://www.udemy.com/course/transformers-nlp/'],
+
+    # Reinforcement Learning
+    ['Reinforcement Learning Specialization','https://www.coursera.org/specializations/reinforcement-learning'],
+    ['Deep Reinforcement Learning 2.0','https://www.udemy.com/course/deep-reinforcement-learning/'],
+
+    # Computer Vision
+    ['Computer Vision Specialization','https://www.coursera.org/specializations/computer-vision'],
+    ['OpenCV Python for Beginners','https://www.udemy.com/course/opencv-python-for-beginners/'],
+    ['Advanced Computer Vision with TensorFlow','https://www.udemy.com/course/advanced-computer-vision-tensorflow/'],
+
+    # AI for Everyone & Intro
+    ['AI For Everyone by Andrew NG [Free]','https://www.coursera.org/learn/ai-for-everyone'],
+    ['Introduction to Artificial Intelligence (AI) [Free]','https://www.edx.org/course/artificial-intelligence-ai']
+]
+
+
+# =========================
+# Software Development Courses
+# =========================
+sd_course = [
+    # Programming Basics
+    ['Python for Everybody Specialization','https://www.coursera.org/specializations/python'],
+    ['Java Programming and Software Engineering Fundamentals','https://www.coursera.org/specializations/java-programming'],
+    ['C++ For C Programmers, Part A','https://www.coursera.org/learn/c-plus-plus-a'],
+
+    # Object-Oriented Programming (OOP)
+    ['Java OOP Programming by Udemy','https://www.udemy.com/course/java-object-oriented-programming/'],
+    ['Python OOP - Object Oriented Programming for Beginners','https://www.udemy.com/course/python-object-oriented-programming-oop/'],
+
+    # Data Structures & Algorithms
+    ['Data Structures and Algorithms Specialization','https://www.coursera.org/specializations/data-structures-algorithms'],
+    ['Mastering Data Structures & Algorithms using C and C++','https://www.udemy.com/course/datastructurescncpp/'],
+    ['Algorithms and Data Structures in Python','https://www.udemy.com/course/algorithms-and-data-structures-in-python/'],
+
+    # Software Engineering & Design Patterns
+    ['Software Engineering Essentials','https://www.edx.org/course/software-engineering-essentials'],
+    ['Design Patterns in Java','https://www.udemy.com/course/java-design-patterns-tutorial/'],
+    ['Software Architecture & Design','https://www.coursera.org/learn/software-design-architecture'],
+
+    # Advanced & Deployment
+    ['Advanced Programming in Python','https://www.udemy.com/course/advanced-python-programming/'],
+    ['CI/CD with Jenkins, Docker, and Kubernetes','https://www.udemy.com/course/cicd-pipeline-with-jenkins-docker-kubernetes/'],
+    ['Cloud Computing for Software Developers','https://www.coursera.org/learn/cloud-computing-foundations']
+]
+
+
+
+# =========================
+# Android Development Courses
+# =========================
+android_course = [
+    ['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+    ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+    ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
+    ['Become an Android Kotlin Developer by Udacity','https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940'],
+    ['Android Basics by Google','https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803'],
+    ['The Complete Android Developer Course','https://www.udemy.com/course/complete-android-n-developer-course/'],
+    ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+    ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+    ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+    ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']
+]
+
+# =========================
+# iOS Development Courses
+# =========================
+ios_course = [
+    ['iOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+    ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+    ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+    ['iOS App Development with Swift Specialization','https://www.coursera.org/specializations/app-development'],
+    ['Mobile App Development with Swift','https://www.edx.org/professional-certificate/curtinx-mobile-app-development-with-swift'],
+    ['Swift Course by LinkedIn','https://www.linkedin.com/learning/subscription/topics/swift-2'],
+    ['Objective-C Crash Course for Swift Developers','https://www.udemy.com/course/objectivec/'],
+    ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+    ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+    ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']
+]
+
+# =========================
+# UI/UX Design Courses
+# =========================
+uiux_course = [
+    ['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+    ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+    ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+    ['UX & Web Design Master Course: Strategy, Design, Development','https://www.udemy.com/course/ux-web-design-master-course-strategy-design-development/'],
+    ['DESIGN RULES: Principles + Practices for Great UI Design','https://www.udemy.com/course/design-rules/'],
+    ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+    ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+    ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+    ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']
+]
+
+# =========================
+# Resume and Interview Videos
+# =========================
+resume_videos = [
+    'https://youtu.be/3agP4x8LYFM',
+    'https://youtu.be/fS_t3yS8v5s',
+    'https://youtu.be/aArb68OBFPg',
+    'https://youtu.be/h-NuvOeWWh0',
+    'https://youtu.be/BdQniERyw8I',
+    'https://youtu.be/Tt08KmFfIYQ',
+    'https://youtu.be/CLUsplI4xMU',
+    'https://youtu.be/bhwEsfXS6y8'
+]
+
+interview_videos = [
+    'https://youtu.be/Tt08KmFfIYQ',
+    'https://youtu.be/KukmClH1KoA',
+    'https://youtu.be/7_aAicmPB3A',
+    'https://youtu.be/1mHjMNZZvFo',
+    'https://youtu.be/WfdtKbAJOmE',
+    'https://youtu.be/wFbU185CvDU',
+    'https://youtu.be/TZ3C_syg9Ow'
+]
